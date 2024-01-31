74536b232bcc9f27d550e65a61d3011e1d788dd4
廣煩君 <87625994+massmess@users.noreply.github.com>
2024-01-31 10:24:41 +0800

    Update leap.js

--- leap.js
+++ leap.js
@@ -13,11 +13,3 @@ function leap(year) {
 
 	return y;
 }
-
-<script>
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95,50,40,0,2*Math.PI);
-ctx.stroke();
-</script> 
