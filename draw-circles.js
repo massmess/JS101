c2c28653fc581a8800d98598e765810b0bf2ecb3
廣煩君 <87625994+massmess@users.noreply.github.com>
2025-10-07 14:24:39 +0800

    Update draw-circles.js
    OK

--- draw-circles.js
+++ draw-circles.js
@@ -1,26 +1,27 @@
 function drawIt() {
-		const canvas = document.getElementById("myCanvas");
-		const ctx = canvas.getContext("2d");
-		let count = 0;
-		let circles = []
+	const canvas = document.getElementById("myCanvas");
+	const ctx = canvas.getContext("2d");
+	const img = document.getElementById("lampTiny");//"./高樓的風(small).jpg";
+	const pattern = ctx.createPattern(img, "repeat");
+	const min = 20;
+	const max = 50;
+	let count = 0;
+	let circles = []
 
-		const interval = setInterval(() => {
-		const img = document.getElementById("lampTiny");//"./高樓的風(small).jpg";
-		const pattern = ctx.createPattern(img, "repeat");
-		const min = 20;
-		const max = 50;
-
+	const interval = setInterval(() => {
 		let x = Math.floor(Math.random() * canvas.width);
 		let y = Math.floor(Math.random() * canvas.height);
 		let r = Math.floor(Math.random() * (max - min + 1)) + min;
+
 		for (const one of circles) {
 			const a = x - one[0];
 			const b = y - one[1];
 			if (Math.sqrt(a*a + b*b) < r + one[2]) return;
 		}
+
 		circles.push([x, y, r]);
 		count = count + 1;
-			
+		
 		ctx.beginPath();
 		ctx.arc(x, y, r, 0, 2 * Math.PI);
 		ctx.stroke();
