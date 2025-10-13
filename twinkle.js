1ed9fa56366e877f85cc455f5f9a28e0a5094cf5
廣煩君 <87625994+massmess@users.noreply.github.com>
2025-10-13 13:51:30 +0800

    Update twinkle.js

--- twinkle.js
+++ twinkle.js
@@ -1,6 +1,5 @@
-function twinkle() {
+function twinkle(d) {
 	let count = 0;
-	const d = document.getElementById("twinkle");
 	save = d.innerHTML;
 	saveColor = d.style.color;
 	tmp = save.slice(0, save.length - "<br>(Be<br>There)".length);
